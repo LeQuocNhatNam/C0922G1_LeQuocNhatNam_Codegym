cd71c376f7e66df133edb67dfeb1928ef3aa7b88
LeQuocNhatNam <lequocnhatnam07041994@gmail.com>
2022-11-23 10:30:58 +0700

    edit

--- Module2/ss11_stack_queue/src/exercise/reversing_array_integers_using_stack/ReversingArrayIntegers.java
+++ Module2/ss11_stack_queue/src/exercise/reversing_array_integers_using_stack/ReversingArrayIntegers.java
@@ -4,16 +4,32 @@ import java.util.Stack;
 
 public class ReversingArrayIntegers {
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        stack.push(5);
-        System.out.println("original stack: " + stack);
-        for (int i = 0; i < stack.size(); i++) {
-            stack.push(stack.pop());
+//        Stack<Integer> stack = new Stack<>();
+//        stack.push(1);
+//        stack.push(2);
+//        stack.push(3);
+//        stack.push(4);
+//        stack.push(5);
+//        System.out.println("original stack: " + stack);
+//        Stack<Integer> newStack = new Stack<>();
+//        int size = stack.size();
+//        for (int i = 0; i < size; i++) {
+//            newStack.push(stack.pop());
+//        }
+//        System.out.println("after reversing: " + newStack);
+//
+        Stack<String> stack = new Stack<>();
+        String str = "CodeGym introduces a new building";
+        String[] arrStr = str.split(" ");
+        for (int i = 0; i < arrStr.length; i++) {
+            stack.push(arrStr[i]);
         }
-        System.out.println("after reversing: " + stack);
+        System.out.println("original stack: " + stack);
+        Stack<String> newStack = new Stack<>();
+        int size = stack.size();
+        for (int i = 0; i < size; i++) {
+            newStack.push(stack.pop());
+        }
+        System.out.println("after reversing: " + newStack);
     }
 }
