aad0d738c9119aa723c09f2e8bf5917fb30923dc
LeQuocNhatNam <lequocnhatnam07041994@gmail.com>
2023-02-23 20:00:06 +0700

    fix comment

--- Module4/case_study/src/main/java/com/example/case_study/repository/facility/IFacilityRepository.java
+++ Module4/case_study/src/main/java/com/example/case_study/repository/facility/IFacilityRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface IFacilityRepository extends JpaRepository<Facility, Integer> {
 
-    @Query(value = "select * from facility where name like :name and facility_type_id like :facilityTypeId and flag = true", nativeQuery = true)
+    @Query(value = "select * from facility where name like :name and facility_type_id = :facilityTypeId and flag = true", nativeQuery = true)
     Page<Facility> findByNameAndFacilityTypeId(@Param("name") String name, @Param("facilityTypeId") int facilityTypeId, Pageable pageable);
 
     @Query(value = "select * from facility where name like :name and flag = true", nativeQuery = true)
